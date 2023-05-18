f68b5f49afdd831bff75bf2dbcd0bbda6a329292
Ahmad Wahana Jaya <119087167+Wannjayy@users.noreply.github.com>
2023-05-18 20:48:00 +0700

    im gonna sleep

--- if4b-laravel/database/seeders/DatabaseSeeder.php
+++ if4b-laravel/database/seeders/DatabaseSeeder.php
@@ -20,8 +20,8 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         $this->call([
-            FakultasSeeder::class,
-            //ProdiSeeder::class
+            // FakultasSeeder::class,
+            // ProdiSeeder::class
         ]);
     }
 }
